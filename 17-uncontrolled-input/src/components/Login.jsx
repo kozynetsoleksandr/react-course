b86e0cb76e68ba46e1b33b5185e015706460ec68
Oleksandr Kozynets <kozynetsoleksandr8@gmail.com>
2025-10-06 14:14:02 +0100

    uncontrolled inputs

---
+++ 17-uncontrolled-input/src/components/Login.jsx
@@ -0,0 +1,31 @@
+function Login() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const userData = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+
+    console.log(userData);
+    alert(JSON.stringify(userData));
+  }
+
+  return (
+    <>
+      <h1>Login form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="username">
+          UserName: <input type="text" name="username" />
+        </label>
+
+        <label htmlFor="password">
+          Password: <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
